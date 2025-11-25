9caefe04ded8619e1713042a36a1cb6f7ffa9e70
Ua3012 <ua3012@outlook.com>
2025-11-25 09:58:44 +0200

    Delete ЗєфаБот+ directory

--- ЗєфаБот+/backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('chat/', views.chat_handler, name='chat_api'),
-    path('clear_mem/', views.clear_memory, name='clear_mem_api'),
-]
